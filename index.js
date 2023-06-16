315f4172d04461e9af407282f51984644a8569c8
양희준 <88187322+YHJ96@users.noreply.github.com>
2023-06-16 16:50:43 +0900

    ss

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const javascript = require("./src/javacript");
+const typescript = require("./src/typescript");
 
 module.exports = {
   ...javascript,
+  overrides: [typescript],
 };
---
+++ src/typescript.js
@@ -0,0 +1,5 @@
+module.exports = {
+  files: ["**/*.ts"],
+  parser: "@typescript-eslint/parser",
+  extends: ["plugin:@typescript-eslint/recommended"],
+};
